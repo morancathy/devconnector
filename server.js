423f61ee9c841d9bb3094321dfc2be937bf8a896
Cat <morancathy@gmail.com>
2021-10-25 15:03:32 -0500

    created auth, posts, profiles, & users test routes, tested in postman

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ connectDB();
 app.get('/', (req, res) => res.send('API Running'));
 
 //define routes
-app.use('/api/users', require('./routes/api/users'))
+
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
