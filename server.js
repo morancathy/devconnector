98034edda7143ea664eab0c9373b8bd88126dfde
Cat <morancathy@gmail.com>
2021-10-25 14:31:15 -0500

    hid mongopw

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db')
+
+//Connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//define routes
+app.use('/api/users', require('./routes/api/users'))
 
 const PORT = process.env.PORT || 5000;
 
