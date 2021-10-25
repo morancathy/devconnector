1efd9b5cbac28ecf145b563ae549201178e22e4a
Cat <morancathy@gmail.com>
2021-10-25 16:32:56 -0500

    user schema and validation

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const connectDB = require('./config/db')
 //Connect database
 connectDB();
 
+// Init Middleware
+app.use(express.json({extended: false})) //in place of bodyparser.json
+
+
 app.get('/', (req, res) => res.send('API Running'));
 
-//define routes
+//Define routes
 
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
