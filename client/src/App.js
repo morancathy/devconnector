b7b3fc65cad3db49b595e815d03ab06673808218
Cat <morancathy@gmail.com>
2022-01-27 16:33:55 -0600

    react router set up with 'Routes' instead of 'Switch'

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" className="container" element={<Register />} />
+        <Route path="/login" className="container" element={<Login />} />
+      </Routes>
+    </Fragment>
+  </Router>
 );
 export default App;
