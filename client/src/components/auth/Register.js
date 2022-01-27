43fa51ad7057f1933f18f36d574f10cac42488fc
Cat <morancathy@gmail.com>
2022-01-27 17:01:10 -0600

    implement onChange and onSubmit for register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -10,6 +10,18 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData;
 
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value }); //dont need {} cause just calling function
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <section className="container">
       <h1 className="large text-primary">Sign Up</h1>
@@ -18,13 +30,22 @@ const Register = () => {
       </p>
       <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="form-group">
           <input
             type="email"
             placeholder="Email Address"
             name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
             required
           />
           <small className="form-text">
@@ -38,6 +59,8 @@ const Register = () => {
             placeholder="Password"
             name="password"
             minLength="6"
+            value={password}
+            onChange={(e) => onChange(e)}
           />
         </div>
         <div className="form-group">
@@ -46,6 +69,8 @@ const Register = () => {
             placeholder="Confirm Password"
             name="password2"
             minLength="6"
+            value={password2}
+            onChange={(e) => onChange(e)}
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
