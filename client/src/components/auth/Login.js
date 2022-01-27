a5cb3b1f557f31c05b0cc055e76e8fc915644f5d
Cat <morancathy@gmail.com>
2022-01-27 17:20:48 -0600

    update href to links on register and login forms. (Below commit shoud say 'create LOGIN page')

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -46,7 +47,7 @@ const Login = () => {
         <input type="submit" className="btn btn-primary" value="Login" />
       </form>
       <p className="my-1">
-        Don't Have An Account? <a href="login.html">Sign In</a>
+        Don't Have An Account? <Link to="/register">Sign Up</Link>
       </p>
     </section>
   );
