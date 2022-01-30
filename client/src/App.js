524190afc50e3dd38445e43d380a8bbdc670ac20
Cat <morancathy@gmail.com>
2022-01-30 12:50:21 -0600

    wrap app with Provider from react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,28 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+//Redux
+import { Provider } from "react-redux"; //surround app with Provider
+import store from "./store";
+
 import "./App.css";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/register" className="container" element={<Register />} />
-        <Route path="/login" className="container" element={<Login />} />
-      </Routes>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/register"
+            className="container"
+            element={<Register />}
+          />
+          <Route path="/login" className="container" element={<Login />} />
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 export default App;
