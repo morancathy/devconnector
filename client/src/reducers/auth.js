b4c92d253ea9caf010a28da3e3e8d71e1e5d1aa7
Cat <morancathy@gmail.com>
2022-02-04 10:14:33 -0600

    define user_loaded and auth_error reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,9 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "../actions/types";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -11,6 +16,13 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true, //token works
+        loading: false,
+        user: payload,
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem("token", payload.token);
       return {
@@ -20,8 +32,10 @@ export default function (state = initialState, action) {
         loading: false,
       };
     case REGISTER_FAIL:
-      localStorage.removeItem("token");
+    case AUTH_ERROR: //both these cases do the same thing
+      localStorage.removeItem("token"); //clears token from local storage
       return {
+        //clears all the auth state
         ...state,
         toke: null,
         isAuthenticated: false,
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 //root reducer
 import { combineReducers } from "redux";
 import alert from "./alert";
+import auth from "./auth";
 
 export default combineReducers({
   alert,
+  auth,
 });
 //add the imported alert to the object that we pass into combineReducers
