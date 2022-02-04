1dd3e5cef0d1aa73f9d3c73085450d9e0f3c5a66
Cat <morancathy@gmail.com>
2022-02-04 10:12:08 -0600

    create try/catch to hit api/auth route.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,32 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
+
+//Load User
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    //sets the token to the header if there is one
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get("/api/auth");
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data, //data sent from this route, which is the user. Sends the payload(user) to the action type(user_loaded) in the reducer
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 // REGISTER USER
 export const register =
