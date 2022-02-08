9b038f5b754770e8b8258819cd2cc4255f81c833
Cat <morancathy@gmail.com>
2022-02-08 16:40:44 -0600

    create profile reducer with get_profile and profile_error

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
+import profiles from "./profiles";
 
 export default combineReducers({
   alert,
   auth,
+  profiles,
 });
 //add the imported alert to the object that we pass into combineReducers
